3ee40e7ab3626df17aeb9ec8164a7b9ade48a74a
LosCrimson <lukas.suslavicius@gmail.com>
2022-11-08 15:20:12 +0200

    Tenth task done, but was unable to rename the folder which is weird some sort of ESRPM error or something either or it is working renaming the fodler is not so critical

--- Desktop/Windows Vista/CodeAcademy/2022-11-11_Javascript_exam/Exam/JS Tasks/Task 10/script.js
+++ Desktop/Windows Vista/CodeAcademy/2022-11-11_Javascript_exam/Exam/JS Tasks/Task 10/script.js
@@ -2,6 +2,17 @@
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
 
+import {composition} from "./modules/math/composition.js";
+import {division} from "./modules/math/division.js";
+import {multiplication} from "./modules/math/multiplication.js";
+import {substraction} from "./modules/math/subtraction.js";
+import {one, two, three, four, five} from "./modules/numbers/numbers.js";
+//In other related files made export on all variables and functions and here i just import them declaring varbiales, functions and file location.
+
+console.log("Hej"); // Programmer signature :P
+
+window.onload=function(){ //onload function so that would not encounter issues with laoding script before the page loads
+
 let a = composition(one, four);
 let b = division(four, two);
 let c = substraction(three, two);
@@ -11,3 +22,5 @@ console.log(a);
 console.log(b);
 console.log(c);
 console.log(d);
+
+}
